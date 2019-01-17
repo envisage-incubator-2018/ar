df22382dfb1a9dc087bc5b7cc559a5bdd51b8523
Tali <30426066+talidemestre@users.noreply.github.com>
2019-01-17 14:39:35 +1100

    fixed misdeclaration

--- VR GAME/file-server/vr-talk/init.js
+++ VR GAME/file-server/vr-talk/init.js
@@ -32,7 +32,7 @@ function initVR(tempRoom) {
   scene = new THREE.Scene();
 
   // Create self player
-  selfPlayer = new Player(true);
+  selfPlayer = new PlayerClass(true);
   
 
   // Apply VR stereo rendering to renderer.
